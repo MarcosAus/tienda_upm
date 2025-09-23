1ac5cb6a2a9e49a7c605b5e410288922c3fdaf2f
Hugo Sánchez <hugopoter0@gmail.com>
2025-09-23 09:52:35 +0200

    1 linea

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Welcome to the ticket module App.\nTicket module. Type 'help' to see commands");
-        
 
     }
 }
