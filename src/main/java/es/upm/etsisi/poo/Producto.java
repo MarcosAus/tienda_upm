6bef7368be1925303c61273ad51fc1dab499037c
mtariqulislamm <mtariqulislamm@gmail.com>
2025-09-23 09:43:21 +0200

    Clase Producto completa sin comentarios

---
+++ src/main/java/es/upm/etsisi/poo/Producto.java
@@ -0,0 +1,59 @@
+package es.upm.etsisi.poo;
+
+public class Producto {
+    private int ID;
+    private int numero;
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public Producto(int numero, int ID, String nombre, String categoria, double precio) {
+        this.numero = numero;
+        this.ID = ID;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+}
+
